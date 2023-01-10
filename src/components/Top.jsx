a0ea0b4d10208c360ce22171c1d22ce5875507fa
Alejandro Salas <asalas.wd@outlook.com>
2023-01-09 22:01:43 -0500

    create Top component

---
+++ src/components/Top.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Top = () => {
+  return(
+    <header>
+      my travel journal.
+    </header>
+  )
+}
+
+export default Top;
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const element = document.getElementById('root');
+const root = ReactDOM.createRoot(element);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
