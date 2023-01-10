e6b2e78ed5862d8ceed328b1421f9976513ae8f4
Alejandro Salas <asalas.wd@outlook.com>
2023-01-10 00:08:50 -0500

    add css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 
 const Card = (props) => {
+  const [id, setId] = useState(props.id);
   const [img, setImg] = useState(props.img);
   const [country, setCountry] = useState(props.country);
   const [map_link, setMap_link] = useState(props.map_link);
@@ -13,12 +14,14 @@ const Card = (props) => {
     <div className='card'>
       <img src={`../../public/${img}`} alt='this place' height='225' width='300'/>
       <div className='destination-content'>
-        <h3>{country}</h3>
-        <a href={map_link} target='blank' >View on Google Maps</a>
+        <div className='location'>
+          <h3>{country}</h3>
+          <a href={map_link} target='blank' >View on Google Maps</a>
+        </div>
         <h2>{place}</h2>
         <h3>{date.start} - {date.end}</h3>
         <p>{description}</p>
-      </div>
+      </div>      
     </div>
   )
 }
